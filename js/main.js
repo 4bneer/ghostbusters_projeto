e7641de2002c1d18412902d51cebc3e986f7f142
Abner Batista <abnerjob16@gmail.com>
2024-05-17 07:07:46 -0300

    Projeto concluido

--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 $(document).ready(function () {
 
-    $('form').submit(function (e) {
-        e.preventDefault();
-        $('#nome').val('')
-        $('#email').val('')
-        $('#adress').val('')
-        $('#more').val('')
-        $('#cpf').val('')
-        $('#cep').val('')
-        $('#telefone').val('')
-    })
-
     $('#cpf').mask('000.000.000-00');
     $('#cep').mask('00000-000');
     $('#telefone').mask('(00) 00000-0000');
@@ -40,12 +29,22 @@ $(document).ready(function () {
                 required: true
             }
         },
+
         messages: {
             nome: 'Por favor, insira seu nome completo',
             email: 'Forneça um endereço de e-mail válido',
             telefone: 'Por favor insira um número de celular válido',
             cpf: 'Insira um CPF válido',
             cep: 'CEP inválido'
+        },
+
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids;
+            if (camposIncorretos === 0) {
+                alert("Formulário enviado com sucesso!");
+            } else {
+                alert("Por favor complete todos os campos corretamente antes de enviar o formulário.");
+            }
         }
 
     })
